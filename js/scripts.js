8b1aa52094c80d3d0994911cc3033208590a8997
Blake Gotting, Cole Marsteller and Devin Sweeting <cole@>
2018-10-18 10:08:36 -0700

    build voter website that displays different information depending on age, add links to relevant information

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
 $(document).ready(function() {
+  var age = parseInt(prompt("How old are you?"));
 
+  if (age >= 18) {
+    $('#voter').show();
+  }
+    else {
+    alert("Now don't go crazy!");
+      $('#minor').show();
+
+
+    }
 });
